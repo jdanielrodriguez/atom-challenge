a03ce6eb41615f9be3a5899ff7c2a3d80ece5db7
jdanielrodriguez <dannyjose1112@hotmail.com>
2025-01-21 21:23:59 -0600

    :hammer::wrench: added first version for task list and detail

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './auth/login-page/login-page.component';
-import { AuthGuard } from './core/guards/auth.guard';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
   { path: 'auth/login', component: LoginPageComponent },
-  { path: 'tasks', loadComponent: () => import('./tasks/task-list-page/task-list-page.component').then((m) => m.TaskListPageComponent), canActivate: [AuthGuard] },
+  { path: 'tasks', loadComponent: () => import('./tasks/task-list-page/task-list-page.component').then((m) => m.TaskListPageComponent), canActivate: [authGuard] },
 ];
---
+++ apps/frontend/src/app/tasks/task-detail-page/task-detail-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-detail-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-detail-page.component.html',
+  styleUrl: './task-detail-page.component.scss'
+})
+export class TaskDetailPageComponent {
+
+}
