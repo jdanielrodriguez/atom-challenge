8cdc7728f8dd0df841c78686e06ba3ce9be03696
jdanielrodriguez <dannyjose1112@hotmail.com>
2025-01-21 21:25:54 -0600

    :hammer::wrench: added first version for register page

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './auth/login-page/login-page.component';
+import { RegisterPageComponent } from './auth/register-page/register-page.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
   { path: 'auth/login', component: LoginPageComponent },
+  { path: 'auth/register', component: RegisterPageComponent },
   { path: 'tasks', loadComponent: () => import('./tasks/task-list-page/task-list-page.component').then((m) => m.TaskListPageComponent), canActivate: [authGuard] },
 ];
