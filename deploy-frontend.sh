02729126f6119b8a5fb473b82750f3be0630257e
jdanielrodriguez <dannyjose1112@hotmail.com>
2025-01-26 18:02:22 -0600

    :rocket: added deploy configuration

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# 1. Compilar el frontend
 echo "Compilando el frontend..."
 cd apps/frontend
 yarn build
 
-# 2. Hacer el deploy desde el contenedor de Firebase
 echo "Haciendo deploy desde el contenedor de Firebase..."
 docker exec -it atom_challenge_firebase firebase deploy --only hosting --public /hosting/browser
